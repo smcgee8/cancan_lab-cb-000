f1df61e21b67438a04e4cdff1c320fdf8325791d
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 03:30:07 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,6 +9,8 @@ class Ability
 
     can :read, Note, user_id: user.id
 
+    can :update, Note, user_id: user.id
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
