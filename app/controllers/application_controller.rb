ba51af5cdfd0d95fb77958f7eba1492b34dc536f
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 03:42:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    @user ||= User.find(session[:user_id])
+    @user ||= User.find(session[:name])
   end
 end
