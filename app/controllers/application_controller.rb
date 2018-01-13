d25379dbeaa767a0e5203fac487c8e9675e18284
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 03:45:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @user ||= User.find(session[:user_id])
+    User.find(session[:user_id])
   end
 end
