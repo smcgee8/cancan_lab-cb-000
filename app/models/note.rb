5e62cf2006fccc40da8029f2cb5473b087c843eb
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 02:52:24 +0000

    automatically backed up by learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,10 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
+
+  def visible_to
+  end
+
+  def visible_to=()
+  end
 end
