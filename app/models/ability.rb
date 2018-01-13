196d5d0a28e09b0ee7cf3d5aa35cdfdb65e25b89
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 03:55:02 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,9 +9,10 @@ class Ability
       note.readers.include?(user)
     end
 
-    can :read, Note, user_id: user.id
-
-    can :update, Note, user_id: user.id
+    unless user.nil?
+      can :read, Note, user_id: user.id
+      can :update, Note, user_id: user.id
+    end
 
     # Define abilities for the passed in user here. For example:
     #
