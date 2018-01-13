6aea34cf95062a80972f06066c130492bb3c5bf0
Sean McGee <sean.a.mcgee@gmail.com>
2018-01-13 02:57:52 +0000

    automatically backed up by learn

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    can :read, Note do |note|
+      #something
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
